edc8ece8dd5f699ced056b32abbb4c9352056644
NeuralNetBot <console.logisaiah@gmail.com>
2024-10-30 09:28:40 -0700

    Basic Item/Weapon/Powerup framework

---
+++ core/src/game/shootergame/Item/MeleeWeapon.java
@@ -0,0 +1,11 @@
+package game.shootergame.Item;
+
+public interface MeleeWeapon {
+    void update(float delta);
+    void renderFirstPerson();
+    void renderThirdPerson();
+    void attackLight();
+    void attackHeavy();
+    void beginBlock();
+    void endBlock();
+}
