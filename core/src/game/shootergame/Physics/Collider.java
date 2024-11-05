29a98338fabe33929f516d437204fa3e5c84ce34
DOakes1988 <122823802+DOakes1988@users.noreply.github.com>
2024-11-05 13:26:48 -0800

    SwordWeapon Collision Box

--- core/src/game/shootergame/Physics/Collider.java
+++ core/src/game/shootergame/Physics/Collider.java
@@ -6,14 +6,15 @@ public class Collider {
     public float height;
     public float radius;
     public boolean isStatic;
-    
+
     public Collider(float x, float y, float radius, ColliderCallback callback) {
         this.x = x; this.y = y;
         this.height = 0.0f;
         this.radius = radius;
         this.callback = callback;
         this.isStatic = true;
-    }  
+    }
+
     public Collider(float x, float y, float radius, ColliderCallback callback, boolean isStatic, float height) {
         this.x = x; this.y = y;
         this.dx = 0.0f; this.dy = 0.0f;
@@ -21,7 +22,7 @@ public class Collider {
         this.radius = radius;
         this.callback = callback;
         this.isStatic = isStatic;
-    }  
+    }
 
     public interface ColliderCallback {
         void callback(Collider collider, float newDX, float newDY);
