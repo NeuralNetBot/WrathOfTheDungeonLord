ac0fcc9e7fed8f75ceed213e3dbffbc67a86fafc
NeuralNetBot <console.logisaiah@gmail.com>
2024-11-06 15:33:16 -0800

    2.5D sprite prototyping

--- core/src/game/shootergame/Renderer/Sprite2_5D.java
+++ core/src/game/shootergame/Renderer/Sprite2_5D.java
@@ -4,7 +4,11 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Sprite2_5D {
     public TextureRegion texture;
+<<<<<<< HEAD
     public float x, y, z;
+=======
+    public float x, y;
+>>>>>>> 2.5D sprite prototyping
     public float height;
     public float width;
 
@@ -17,18 +21,33 @@ public class Sprite2_5D {
     public boolean isVis;
     public float getDst() { return dst; }
     
+<<<<<<< HEAD
     public Sprite2_5D(TextureRegion texture, float x, float y, float z, float height, float width) {
         this.texture = texture;
         this.textureCalc = new TextureRegion(texture);
         this.x = x; this.y = y; this.z = z;
+=======
+    public Sprite2_5D(TextureRegion texture, float x, float y, float height, float width) {
+        this.texture = texture;
+        this.textureCalc = new TextureRegion(texture);
+        this.x = x; this.y = y;
+>>>>>>> 2.5D sprite prototyping
         this.height = height;
         this.width = width;
     }
 
+<<<<<<< HEAD
     public Sprite2_5D(TextureRegion texture, float x, float y, float z, float height) {
         this.texture = texture;
         this.textureCalc = new TextureRegion(texture);
         this.x = x; this.y = y; this.z = z;
+=======
+    public Sprite2_5D(TextureRegion texture, float x, float y, float height) {
+        this.texture = texture;
+        this.textureCalc = new TextureRegion(texture);
+        this.x = x;
+        this.y = y;
+>>>>>>> 2.5D sprite prototyping
         this.height = height;
         float aspect = Math.abs(texture.getU2() - texture.getU()) / Math.abs(texture.getV2() - texture.getV());
         this.width = height * aspect;
