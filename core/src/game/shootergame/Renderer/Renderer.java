0141288cf7874c513b3cdb36c5218b18aeb8993b
NeuralNetBot <console.logisaiah@gmail.com>
2024-11-05 23:16:32 -0800

    prototpying sprite draw system

--- core/src/game/shootergame/Renderer/Renderer.java
+++ core/src/game/shootergame/Renderer/Renderer.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedList;
+
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.atomic.AtomicBoolean;
