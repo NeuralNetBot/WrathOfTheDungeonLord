d5f7a733afe72578627b2225c5682fbebc8c294a
NeuralNetBot <console.logisaiah@gmail.com>
2024-11-15 15:10:02 -0800

    remote player no has rendered sprite

--- core/src/game/shootergame/Network/RemotePlayer.java
+++ core/src/game/shootergame/Network/RemotePlayer.java
@@ -1,18 +1,33 @@
 package game.shootergame.Network;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+import game.shootergame.ShooterGame;
+import game.shootergame.Renderer.Renderer;
+import game.shootergame.Renderer.Sprite2_5D;
+
 public class RemotePlayer {
     float x, y;
     float dx, dy;
+    Sprite2_5D sprite;
 
     RemotePlayer() {
+        ShooterGame.getInstance().am.load("debugtex.png", Texture.class);
+        ShooterGame.getInstance().am.finishLoading();
+        Texture tex = ShooterGame.getInstance().am.get("debugtex.png", Texture.class);
+        TextureRegion reg = new TextureRegion(tex, 0, 0, 1024, 1024);
 
+        sprite = new Sprite2_5D(reg, x, y, -1.0f, 3.0f, 0.5f);
+        Renderer.inst().addSprite(sprite);
     }
 
     public void update(float delta) {
         x = x + dx * delta;
         y = y + dy * delta;
-
-        System.out.println(x + " " + y);
+        
+        sprite.x = x;
+        sprite.y = y;
     }
 
     public void updateNetwork(float x, float y, float dx, float dy) {
@@ -23,6 +38,6 @@ public class RemotePlayer {
     }
 
     public void kill() {
-
+        Renderer.inst().removeSprite(sprite);
     }
 }
